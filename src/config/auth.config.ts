a9949d19d8a0bd5d81af0f1638525d0f1e26bc35
Karan Kumar Sah <krotrn.ks@gmail.com>
2025-09-16 17:00:28 +0000

    added authentication

---
+++ src/config/auth.config.ts
@@ -0,0 +1,67 @@
+import { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import Google, { GoogleProfile } from "next-auth/providers/google";
+
+export const authConfig: NextAuthConfig = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile: async (profile: GoogleProfile) => {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    }),
+    Credentials({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Enter your email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+
+      authorize: async (credentials) => {
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "credentials") {
+        return true;
+      }
+      if (!user) {
+        return false;
+      }
+      return true;
+    },
+    async jwt({ token, user }) {
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login", // Custom login page
+    signOut: "/", // Redirect after sign out
+    error: "/", // Error page for authentication errors
+    newUser: "/register", // New user registration page
+  },
+};
