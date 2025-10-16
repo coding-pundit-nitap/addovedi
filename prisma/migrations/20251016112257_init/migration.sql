4ddd317f4dab5cd6fe31d9436a36cb795f71776f
Karan Kumar Sah <krotrn.ks@gmail.com>
2025-10-16 11:23:52 +0000

    migration add

---
+++ prisma/migrations/20251016112257_init/migration.sql
@@ -0,0 +1,100 @@
+-- CreateEnum
+CREATE TYPE "SponsorTier" AS ENUM ('Gold', 'Silver', 'Bronze', 'Platinium', 'Premier');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "college" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "department" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "entry_fee" INTEGER NOT NULL,
+    "branch" TEXT NOT NULL,
+    "schedule" TEXT NOT NULL,
+    "eventDate" TIMESTAMP(3),
+    "eventTime" TEXT,
+    "location" TEXT,
+    "tag" TEXT,
+    "teamSize" TEXT,
+    "prizeMoney" INTEGER,
+    "rules" JSONB,
+    "guidelines" JSONB,
+    "resultAnnouncement" TEXT,
+    "contact" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Sponsor" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "logoUrl" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "tier" "SponsorTier" NOT NULL,
+
+    CONSTRAINT "Sponsor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ScienceExhibition" (
+    "id" TEXT NOT NULL,
+    "schoolName" TEXT NOT NULL,
+    "teacherCoordinator" TEXT NOT NULL,
+    "teacherPhone" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ScienceExhibition_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Participant" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "class" TEXT NOT NULL,
+    "exhibitionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_UserEvents" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_UserEvents_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "_UserEvents_B_index" ON "_UserEvents"("B");
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_exhibitionId_fkey" FOREIGN KEY ("exhibitionId") REFERENCES "ScienceExhibition"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserEvents" ADD CONSTRAINT "_UserEvents_A_fkey" FOREIGN KEY ("A") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserEvents" ADD CONSTRAINT "_UserEvents_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
